875780f91fe8fad6885342b2b130bdb3c667680a
Brendan King <brendan@brendanking.com>
2026-05-31 00:16:01 -0400

    More constraint data generation and some minor error check changes

--- Assets/Constraint.cs
+++ Assets/Constraint.cs
@@ -51,10 +51,22 @@ public class Constraint
                 constraintData = new int[] { Random.Range(0, 2), Random.Range(0, 4) }; // Needle color (Red = 0, Black = 1) | Direction of needle (North = 0, East = 1, South = 2, West = 3)
                 break;
             case ConstraintType.Domino:
-                constraintData = new int[] { Random.Range(0, 9), Random.Range(0, 9) }; // Number of top pips | Number of bottom pips
+                constraintData = new int[] { Random.Range(0, 2), Random.Range(0, 10), Random.Range(0, 10) }; // Pip color (White = 0, Black = 1) | Number of top pips | Number of bottom pips
                 break;
             case ConstraintType.Gear:
                 constraintData = new int[] { Random.Range(0, 3) }; // Gear spin direction (Clockwise = 0, Counter-clockwise = 1, Stationary = 2)
+                break;
+            case ConstraintType.Matrix:
+                constraintData = new int[] { Random.Range(0, 10) }; // Number of lit squares
+                break;
+            case ConstraintType.PCB:
+                constraintData = new int[] { Random.Range(0, 2), Random.Range(0, 11) }; // Chip relative to board (Square = 0, 45 degrees = 1) | Trace layout (0 = │, 1 = ─, 2 = ┌, 3 = ┐, 4 = └, 5 = ┘, 6 = ├, 7 = ┤, 8 = ┬, 9 = ┴, 10 = ┼)
+                break;
+            case ConstraintType.Screws:
+                constraintData = new int[] { Random.Range(0, 2), Random.Range(1, 10) }; // Screw type (Phillips = 0, Torx = 1) | Number of screws
+                break;
+            case ConstraintType.StampMarkings:
+                constraintData = new int[] { Random.Range(0, 3) }; // Present markings (CLASSIFIED = 0, DECLASSIFIED = 1, CLASSIFIED & DECLASSIFIED = 2)
                 break;
         }
     }
@@ -68,8 +80,6 @@ public class Constraint
         switch (constraintType)
         {
             case ConstraintType.Compass:
-                if (constraintData.Length != 2)
-                    return null;
                 int[] releventSegments = GetCompassSegmentIndexes();
                 if (releventSegments == null)
                     return null;
@@ -101,9 +111,11 @@ public class Constraint
 
     // Gets the reading order indexes of each relevent segment for the compass's direction and position
     // Indexes greater than 6 refer to the segments of the right digit
-    // Returns null if the constraint position or data is an unexpected value
+    // Returns null if the constraint type, data or position is an unexpected value
     int[] GetCompassSegmentIndexes()
     {
+        if (constraintType != ConstraintType.Compass || constraintData.Length != 2)
+            return null;
         switch (constraintPosition)
         {
             case ConstraintPosition.TopLeft:
